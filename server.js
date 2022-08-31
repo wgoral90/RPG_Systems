fee97ccbfc0bbed9161c481b29cec1f0d02e6af5
wgoral90 <81937209+Iskrafir@users.noreply.github.com>
2022-08-31 21:17:09 +0200

    server.js fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').parse()
-}
+// if (process.env.NODE_ENV !== "production") {
+// require('dotenv').parse()
+// }
 
 const express = require("express")
 const app = express()
