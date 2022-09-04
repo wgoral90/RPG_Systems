287c745ccfbd777be2bc2ac9486b3bae6a10e415
wgoral90 <81937209+Iskrafir@users.noreply.github.com>
2022-09-04 22:24:01 +0200

    process.env change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// if (process.env.NODE_ENV !== "production") {
-// require('dotenv').parse()
-// }
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').load()
+}
 
 const express = require("express")
 const app = express()
